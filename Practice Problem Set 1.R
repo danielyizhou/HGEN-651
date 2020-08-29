58abb75932d7b45df8e59f5abb2ec3b54ebc7aa5
Daniel Zhou <daniel.yi.zhou@gmail.com>
2020-08-29 18:18:56 -0400

    finished question 1

--- Practice Problem Set 1.R
+++ Practice Problem Set 1.R
@@ -1,8 +1,49 @@
 
 
 # Question 1 --------------------------------------------------------------
+  #Using Matrix A below, find A-1, showing your working with determinant, cofactor and adjoint matrices, and 
+  #then demonstrate that AA-1 = I, where I is an identity matrix.
+  # A =	4	8	9
+  #     2	1	6
+  #     7	5	2
 
-
+    #step 1 - find the determinant of matrix A
+    A <- matrix(c(4,2,7,8,1,5,9,6,2), 3, 3) #make the matrix specified in the question
+    A_det <- det(A)  #use det() function to find the determinant.
+    A_det #since this value is non-zero, there is an inverse to be found.
+    
+    #step 2 - create the co-factor matrix 
+    matrix_indices <- c(1,2,3) #setup for function
+    #write a function to return the cofactor of each element in the matrix
+    cofactor <- function (matrix, i, j){
+      submatrix_rows <- matrix_indices[-i]
+      submatrix_columns <- matrix_indices[-j]
+      submatrix <- matrix[submatrix_rows, submatrix_columns]
+      signed_minor <- (-1)^(i+j) * det(submatrix)
+      return(signed_minor)
+    }
+    
+    #make a vector of all cofactors in the matrix
+    cofactor_values <- NULL
+    for (column in 1:3) {
+      for (row in 1:3){
+        cofactor_values <- c(cofactor_values, cofactor(A, row, column))
+      }
+    }
+    #create the cofactor matrix 
+    cofactor_matrix <- matrix(cofactor_values, 3, 3)
+    
+    #step 3 - create the adjunct matrix
+    adjunct_matrix <- t(cofactor_matrix) #simply transpose the cofactor matrix to get the adjunct matrix
+    
+    #step 4 - divide each element of the adjunct matrix by the determinant to get the inverse matrix
+    A_inverse_matrix <- 1/A_det * adjunct_matrix
+    
+    #step 5 - confirm that the matrix * inverse matrix = identity matrix
+    
+    matrix_check <- A %*% A_inverse_matrix
+    matrix_check #the result is very very close to a 3x3 identity matrix
+    
 # Question 2 --------------------------------------------------------------
 
   #Sarah and Adam plan to play a series of 5 tennis matches.  They agree that the overall winner must win at 
